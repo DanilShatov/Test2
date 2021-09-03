538aa937b7c34371019f24a838b916c25d99cf09
Shatov <topprogrammerjava@gmail.com>
2021-09-03 22:46:46 +0300

    add eclipse workspace
    Signed-off-by: Shatov <topprogrammerjava@gmail.com>

---
+++ workspace/test1/src/test1/Main.java
@@ -0,0 +1,15 @@
+package test1;
+
+public abstract class Main {
+
+	public static void main(String[] args) {
+	
+	}
+
+	
+	public interface privet {
+	
+		
+	}
+
+}
---
+++ workspace/test1/src/test1/in1.java
@@ -0,0 +1,8 @@
+package test1;
+
+public interface in1 {
+default void function() {
+System.out.println("hellow");	
+}
+public void privet();
+}
